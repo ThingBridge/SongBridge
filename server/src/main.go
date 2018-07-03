d6073abeca6dda38c61c217e15992ee9e7a29c03
Kevin <accounts_kkla@icloud.com>
2018-07-03 19:36:04 +0200

    Route hinzugefügt

--- server/src/main.go
+++ server/src/main.go
@@ -5,6 +5,36 @@ import (
 	"net/http"
 )
 
+func handleLinkRequest(responseWriter http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+
+	id := query.Get("id")
+	source := query.Get("source")
+	kind := query.Get("kind")
+
+	if id == "" {
+		responseWriter.WriteHeader(http.StatusBadRequest)
+		responseWriter.Write([]byte("Missing query parameter 'id'"))
+		return
+	}
+
+	if source == "" {
+		responseWriter.WriteHeader(http.StatusBadRequest)
+		responseWriter.Write([]byte("Missing query parameter 'source'"))
+		return
+	}
+
+	if kind == "" {
+		responseWriter.WriteHeader(http.StatusBadRequest)
+		responseWriter.Write([]byte("Missing query parameter 'kind'"))
+		return
+	}
+
+	var response = id + " " + source + " " + kind
+	responseWriter.Write([]byte(response))
+}
+
 func main() {
+	http.HandleFunc("/link", handleLinkRequest)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
