06b114f8e8ccb1b0287e2d90c4c5fe6dba43d202
Kevin <accounts_kkla@icloud.com>
2018-07-03 19:11:39 +0200

    Ausgabe geändert

--- server/src/main.go
+++ server/src/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Print("Server is running")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
